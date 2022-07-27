4683ab161642189b4889cd01084c2884f01a787a
IK <75577833+IndrayaniKulkarni@users.noreply.github.com>
2022-07-27 20:08:35 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,34 @@
+//import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom"; 
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import  Navbar from "./components/navbar.component.js";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+import ApexChart from "./components/time_chart.component";
+
+import './App.css';
+//react component
+function App() {
+  return (
+    //below -component need to expand - describe
+    <Router>
+      <div className="container">
+      <Navbar />
+       <br />
+       <Route path="/" exact component ={ExercisesList} />
+       <Route path="/edit/:id" component ={EditExercise} />
+       <Route path="/create" component={CreateExercise} />
+       <Route path="/user" component={CreateUser} />
+       <Route path="/graph/:username" component={ApexChart} />
+      </div>
+      
+    </Router> 
+  );
+}
+
+export default App;
+
